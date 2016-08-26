ba14996c345c02c80c609b7e7e60f1abb6a4784b
spectrecode <you@example.com>
2016-08-26 10:21:16 -0500

    aprendiendo pares

---
+++ main.js
@@ -0,0 +1,16 @@
+var num = prompt("Ingrese un número");
+	if (num % 2 == 0) {
+		alert("El número es par");
+	}
+	else {
+		alert("el número es impar");
+	};
+
+
+	var promediado=puntosSquad*maxPuntos/100;
+    if(promediado>=0.8) {
+    return "Muy bien squad!";
+    }
+    else {
+        return "Pueden hacerlo mejor.";
+    }
---
+++ readme.md
@@ -0,0 +1,8 @@
+#PAR E IMPAR
+* inicio
+* leer datos
+* verificar si es par o impar
+* dar respuesta
+* fin
+
+![Sin titulo](img/ej2.jpg)
